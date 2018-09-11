4830b2c4f8c9c7c9e3c9b102dafe6583a50ce303
tianxin <844577216@qq.com>
2018-09-11 10:59:00 +0800

    反射方法

--- index.php
+++ index.php
@@ -6,8 +6,17 @@ require __DIR__.'/vendor/autoload.php';
 use app\Controller;
 use app\Model;
 use app\searchModel\SearchModel;
+use app\MyReflectionMerhod;
+
+$reflect=new MyReflectionMerhod(Controller::class,'__construct');
+print_r($reflect);
 
 
 
 
-call_user_func([new Controller(new SearchModel()),'getName']);
+
+
+//call_user_func([new Controller(new SearchModel()),'getName']);
+
+
+
